5625e296da2aea8f97981c9481b36257847bcea5
ismail kupik <92760309+kupikismail@users.noreply.github.com>
2022-10-29 19:19:27 +0300

    added base character values options to players

--- game.py
+++ game.py
@@ -177,8 +177,22 @@ class Merchant():  #Merchant class has been created and the following features (
         self.weapon_point_left= self.starting_weapon_point
         self.armor_point_left = self. starting_armor_point
         self.faction_Orcs = None  
+        self.Orcs_unit_number = 50
+        self.Orcs_attack_point = 30
+        self.Orcs_health_point= 150
+        self.Orcs_regeneration_number = 10
+
         self.faction_Dwarves = None
+        self.Dwarves_unit_number = 50
+        self.Dwarves_attack_point = 30
+        self.Dwarves_health_point= 150
+        self.Dwarves_regeneration_number = 10
+
         self.faction_Elves = None 
+        self.Elves_unit_number = 50
+        self.Elves_attack_point = 30
+        self.Elves_health_point= 150
+        self.Elves_regeneration_number = 10
     
     def AssignFactions(self): #The factions in the game are assigned for Merchant to use.
         self.faction_Orcs= Orcs
@@ -211,11 +225,135 @@ class Merchant():  #Merchant class has been created and the following features (
     def EndTurn(self):
         self.weapon_point_left= self.starting_weapon_point
         self.armor_point_left = self. starting_armor_point
+    
+    def start_game(self):
+        game_status = 1  #This indicates the status of the game. When this equals 0, the game ends, if it is equal to 1, the game continues, if it is equal to 2, the new game starts
+        print("Welcome to the 'The Warmonger: A New Dimension' Game ")
+        print("Before starting the game, you can change some features in the game.")
+        print("You can change the unit numbers, attack points, health points, unit regeneration numbers of Orcs, Dwarves and Elves. You can also change your weapon points and armor points that you will get each round.")
+        print("If there is a change you want to make, write 1 in the terminal, otherwise write 0.")
+        game_change_request = input()
+        if int(game_change_request) == 0:
+            pass
+        elif int(game_change_request)== 1:
+            while int(game_change_request) ==1:  #If the players want to make changes about the values and powers of the characters, they can do it thanks to this while.
+                print("Specify which character you want to change. Write 1 for Merchant,  2 for Orcs, 3 for Drawres,4 for Elves, 5 for starting game.")
+                selected_character = input()
+                if int(selected_character) == 1:
+                    print("Press 1 to change weapon point, press 2 to change armor point. The recommended amount for both is 10.")
+                    decision_for_Merchant = input()
+                    if int(decision_for_Merchant) == 1:
+                        print("Determine the amount")
+                        self.starting_weapon_point == int(input())
+                    elif decision_for_Merchant == 2:
+                        self.starting_armor_point == int(input())
+                    else:
+                        print("There is no such option, write 1 if you want to make another changes, press 0 if not")
+                        user_desicion = input()
+                        if int(user_desicion) == 1:
+                            pass
+                        elif int(user_desicion)  == 0 :
+                            game_change_request == 0 
+
+                elif int(selected_character) == 2:
+                    print("Write 1 to change the number of units, 2 to change the attack point, 3 to change the health point, and 4 to change the unit regeneration number.")
+                    user_desicion = input()
+                    if int(user_desicion) == 1:
+                        print("Enter the amount you want. The recommended number is 50")
+                        self.Orcs_unit_number = int(input())
+                    elif int(user_desicion) ==2:
+                        print("Enter the amount you want. The recommended number is 30")
+                        self.Orcs_attack_point= int(input())
+                    elif int(user_desicion)==3:
+                        print("Enter the amount you want. The recommended number is 150")
+                        self.Orcs_health_point= int(input())
+                    elif int(user_desicion) == 4:
+                        print("Enter the amount you want. The recommended number is 10")
+                        self.Orcs_regeneration_number= int(input())
+                    else:
+                        print("There is no such option, write 1 if you want to make another changes, press 0 if not")
+                        user_desicion = input()
+                        if int(user_desicion) == 1:
+                            pass
+                        elif int(user_desicion) == 0 :
+                            game_change_request == 0 
+
+                elif int(selected_character) == 3:
+                    print("Write 1 to change the number of units, 2 to change the attack point, 3 to change the health point, and 4 to change the unit regeneration number.")
+                    user_desicion = input()
+                    if int(user_desicion) == 1:
+                        print("Enter the amount you want. The recommended number is 50")
+                        self.Drawres_unit_number = int(input())
+                    elif int(user_desicion) ==2:
+                        print("Enter the amount you want. The recommended number is 30")
+                        self.Drawres_attack_point= int(input())
+                    elif int(user_desicion)==3:
+                        print("Enter the amount you want. The recommended number is 150")
+                        self.Drawres_health_point= int(input())
+                    elif int(user_desicion) == 4:
+                        print("Enter the amount you want. The recommended number is 10")
+                        self.Drawres_regeneration_number= int(input())
+                    else:
+                        print("There is no such option, write 1 if you want to make another changes, press 0 if not")
+                        user_desicion = input()
+                        if int(user_desicion) == 1:
+                            pass
+                        elif int(user_desicion) == 0 :
+                            game_change_request == 0 
+
+                elif int(selected_character) == 4:
+                    print("Write 1 to change the number of units, 2 to change the attack point, 3 to change the health point, and 4 to change the unit regeneration number.")
+                    user_desicion = input()
+                    if int(user_desicion) == 1:
+                        print("Enter the amount you want. The recommended number is 50")
+                        self.Elves_unit_number = int(input())
+                    elif int(user_desicion) ==2:
+                        print("Enter the amount you want. The recommended number is 30")
+                        self.Elves_attack_point= int(input())
+                    elif int(user_desicion)==3:
+                        print("Enter the amount you want. The recommended number is 150")
+                        self.Elves_health_point= int(input())
+                    elif int(user_desicion) == 4:
+                        print("Enter the amount you want. The recommended number is 10")
+                        self.Elves_regeneration_number= int(input())
+                    else:
+                        print("There is no such option, write 1 if you want to make another changes, press 0 if not")
+                        user_desicion = input()
+                        if int(user_desicion) == 1:
+                            pass
+                        elif int(user_desicion) == 0 :
+                            game_change_request == 0 
+                
+                elif int(selected_character) == 5 : 
+                    game_change_request = 0 
+                
+                else: 
+                    print("There is no such option, write 1 if you want to make another changes, press 0 if not")
+                    user_desicion = int(input())
+                    if user_desicion == 1:
+                        pass
+                    
+                    elif user_desicion == 0 :
+                        game_change_request == 0
+
+
+                
+                
+                
+
+
+
+
+
+                        
+
+
 
 
     
     
-
+A = Merchant()
+A.start_game()
 
 
 
